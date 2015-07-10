d82eb86590e1290b23b516820090f559e3598933
Aplicticus <D201117@edu.rocwb.nl>
2015-07-10 20:51:17 +0200

    Add frmLogin, Sorted in map

--- StProgress/StProgress/ProgramStart/frmLogin.Designer.cs
+++ StProgress/StProgress/ProgramStart/frmLogin.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace StProgress
 {
-    partial class Form1
+    partial class frmLogin
     {
         /// <summary>
         /// Required designer variable.
@@ -28,9 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.ClientSize = new System.Drawing.Size(384, 212);
+            this.ControlBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
